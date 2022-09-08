7f52f64ddad52602f0e20812af8420e2cedf28d3
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2022-09-09 01:28:24 +0530

    updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HorizontalFormComponent } from './horizontal-form/horizontal-form.component';
-import { CompareConactsComponent } from './horizontal-form/components/compare-conacts/compare-conacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HorizontalFormComponent,
-    CompareConactsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

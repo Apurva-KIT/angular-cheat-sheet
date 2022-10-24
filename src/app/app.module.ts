7c4b312d6fafd3c43fc22186a41d13a6b3d40bf1
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2022-10-24 11:15:17 +0530

    ACS-17 resolved conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,29 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'src/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import { LoginFormComponent } from './authentication/login/login-form/login-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    PageNotFoundComponent,
+    PageUnderConstructionComponent,
+    LoginFormComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

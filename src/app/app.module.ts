e8d9095d63ec6f09cfb38deb79203476cbbe8f04
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2022-11-18 17:38:43 +0530

    ACS-18 configuring tsconfig.json file for shorthand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from 'src/shared/shared.module';
+import { SharedModule } from '@shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './authentication/login/login-form/login-form.component';

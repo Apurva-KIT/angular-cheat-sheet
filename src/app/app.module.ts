40083d9b0618cee6b1a59a674f0a7d809d2e09ad
Apurva Yadav <Apurva-KIT@users.noreply.github.com>
2022-11-18 13:53:57 +0530

    ACS-18 Intial folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from 'src/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './authentication/login/login-form/login-form.component';
+import { RegistrationFormComponent } from './authentication/register/registration-form/registration-form.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PageUnderConstructionComponent } from './page-under-construction/page-under-construction.component';
@@ -18,6 +18,7 @@ import { PageUnderConstructionComponent } from './page-under-construction/page-u
     PageNotFoundComponent,
     PageUnderConstructionComponent,
     LoginFormComponent,
+    RegistrationFormComponent,
   ],
   imports: [
     BrowserModule,
